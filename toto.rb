8dacd5b8de0cb9d65b4ecc24acf6fdf969d9bbfc
soranotenshi <sora_no_tenshi@hotmail.com>
2013-12-19 01:57:29 +0100

    simple sum

--- toto.rb
+++ toto.rb
@@ -1,1 +1,11 @@
-test
+puts "gimme a number"
+number = gets
+number = number.chomp
+puts "and another"
+number2 = gets
+number2 = number2.chomp
+sum = (number.to_i + number2.to_i)
+puts "-----------------------"
+puts number + "+" + number2 + " equals " + (sum.to_s) +". Fucking A"
+puts "-----------------------"
+
